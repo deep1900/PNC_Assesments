d1415edee4de47f0ea52c98ab2fb2e186c040b99
Deep Patel <deepp@virtusa.com>
2020-03-19 12:51:33 -0400

    userAggrigate Initiated

---
+++ UserAgrigateService/src/main/java/com/demo/UserAgrigateService/UserAgrigateServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.UserAgrigateService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAgrigateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAgrigateServiceApplication.class, args);
+	}
+
+}
---
+++ UserAgrigateService/src/test/java/com/demo/UserAgrigateService/UserAgrigateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.UserAgrigateService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAgrigateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
